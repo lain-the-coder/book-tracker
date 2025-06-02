5af15fbd057f54242d53bd2239d463e52a5dc955
Ehab Ahmed <ahmed.ehabj@gmail.com>
2025-06-03 01:57:13 +0400

    Added GET ALL method

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome to lain's Book Tracker App!"
+# Get all route
+@app.route('/books', methods=['GET'])
+def get_books():
+    conn = sqlite3.connect('books.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM books')
+    rows = cursor.fetchall()
+    conn.close()
 
-if __name__ == "__main__":
+    books = []
+    for row in rows:
+        book = {
+            'id': row[0],
+            'title': row[1],
+            'author': row[2],
+            'genre': row[3],
+            'status': row[4]
+        }
+        books.append(book)
+    return jsonify(books)
+
+if __name__ == '__main__':
     app.run(debug=True)
